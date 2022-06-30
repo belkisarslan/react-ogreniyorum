79498a00b7343fff46856dde1899efb4ec5abc1d
belkisarslan <belkisarslan33@gmail.com>
2022-06-30 17:27:38 +0300

    props and props child class component

--- props/src/components/Collapse.js
+++ props/src/components/Collapse.js
@@ -1,20 +1,44 @@
 import React from "react";
 
-const Collapse = (props) => {
-    console.log(props)
-    return (
-        <div>
-                <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-                  {props.collapseText}
-                </a>
-            
-            <div class="collapse" id="collapseExample">
-                {props.children}
-                Linke tıklandı.
-            </div>
+//fonksiyon versiyon
+// const Collapse = (props) => {
+//     console.log(props)
+//     return (
+//         <div>
+//                 <a className="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+//                   {props.collapseText}
+//                 </a>
 
-        </div>
-    )
+//             <div className="collapse" id="collapseExample">
+//                 {/* props children seçtik */}
+//                 {props.children}
+//                 Linke tıklandı.
+//             </div>
+
+//         </div>
+//     )
+// }
+
+//class versiyon
+
+class Collapse extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <a className="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                    {this.props.collapseText}
+                    
+                </a>
+
+                <div className="collapse" id="collapseExample">
+                    {this.props.children}
+                    Linke tıklandı.
+                </div>
+
+            </div>
+        )
+    }
 }
 
 export default Collapse
--- props/src/index.js
+++ props/src/index.js
@@ -6,14 +6,16 @@ import Collapse from "./components/Collapse";
 const App = () => {
   return(
     <div className="card">
-
+      
       <Collapse collapseText="Tıklayınız">
+        {/* collapsein içine kart yerleştirmek için açılış kapanış yaptık */}
          <Card cardTitle="İçteki Kart" cardText="3.Kart İçeriği" updateTime="3 dakika önce güncellendi"/>
       </Collapse>
 
       <Card cardTitle="1.Kart" cardText="1.Kart İçeriği" updateTime="3 dakika önce güncellendi"/>
 
       <Card cardTitle="2.Kart" cardText="2.Kart İçeriği" updateTime="1 dakika önce güncellendi" />
+
     </div>
 
 
